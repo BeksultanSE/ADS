f459cd38e130011f73298c8f5d4495ab505da3c9
Beksultan Aitkazy <230599@astanait.edu.kz>
2024-04-05 09:04:36 +0500

    First init

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int task = sc.nextInt();
+        switch (task) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+        }
+        System.out.println("Program is finished!");
+        sc.close();
+    }
+}
