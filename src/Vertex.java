6f5abacd73bc4bedf9a6995ed5ab91440873f81a
Beksultan Aitkazy <230599@astanait.edu.kz>
2024-05-22 18:38:41 +0500

    lab4

---
+++ src/Vertex.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices;
+    public Vertex(V data) {
+        this.data = data;
+        this.adjacentVertices = new HashMap<>();
+    }
+
+    public V getData() {
+        return data;
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination) {
+        adjacentVertices.put(destination, 1.0);
+    }
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex<?> vertex = (Vertex<?>) o;
+        return Objects.equals(getData(), vertex.getData());
+    }
+}
